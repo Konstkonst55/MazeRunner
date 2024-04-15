e93a9dd60b94377f0a45881578b8ce21380f6e79
kxnst <ip311s03>
2024-04-15 18:03:58 +0700

    Добавьте файлы проекта.

---
+++ MazePathFinder/MazePathFinder.cpp
@@ -0,0 +1,10 @@
+﻿// MazePathFinder.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnMazePathFinder()
+{
+}
