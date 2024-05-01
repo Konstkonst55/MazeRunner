f98857c2b81c742663bfb33a0899d9731afb4cd1
kxnst <ip311s03>
2024-05-01 22:22:12 +0700

    Отключена предварительная компиляция

--- MazePathFinder/MazePathFinder.cpp
+++ MazePathFinder/MazePathFinder.cpp
@@ -1,10 +1,1 @@
-﻿// MazePathFinder.cpp : Определяет функции для статической библиотеки.
-//
-
-#include "pch.h"
-#include "framework.h"
-
-// TODO: Это пример библиотечной функции.
-void fnMazePathFinder()
-{
-}
+﻿
